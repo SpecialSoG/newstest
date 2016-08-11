5613f64499a4ca6f657dec553063fc259b1f708b
SpecialSoG <specialist@tut.by>
2016-08-11 22:32:53 +0300

    Changed

--- TestNews/urls.py
+++ TestNews/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-	url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', admin.site.urls),
 	url(r'^$', views.post_list, name='post_list'),
 	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 	url(r'^post/new/$', views.post_new, name='post_new'),
